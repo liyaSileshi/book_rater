a1fc4d436f8c7c326bea9b833f1b82253e556315
liyaSileshi <liyasileshi@gmail.com>
2019-12-06 00:10:36 -0800

    added templates

--- book_rater/rater/views.py
+++ book_rater/rater/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Book
 # Create your views here.
+def index(request):
+    all_books = Book.objects.all()
+    context = {'all_books': all_books}
+    return render(request, 'rater/index.html', context)
+
+
+def detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'rater/detail.html', {'book': book})
+    
