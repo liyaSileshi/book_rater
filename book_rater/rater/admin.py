bfd79b84a01ebdcc85be354eca76eb78620e79ef
liyaSileshi <liyasileshi@gmail.com>
2019-12-05 22:45:13 -0800

    added rating

--- book_rater/rater/admin.py
+++ book_rater/rater/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Book
-# from .models import Comment
+from .models import Comment
 
 admin.site.register(Book)
-# admin.site.register(Comment)
+admin.site.register(Comment)
--- book_rater/rater/models.py
+++ book_rater/rater/models.py
@@ -6,5 +6,15 @@ class Book(models.Model):
     description = models.TextField(default='book description')
 
 class Comment(models.Model):
+    RATING_RANGE = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5')
+    )
     comment = models.TextField(default='book comment')
+    rating = models.IntegerField(choices=RATING_RANGE, default=RATING_RANGE[0][0])
     book = models.ForeignKey(Book, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.comment
