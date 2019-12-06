f6978dbe70865cb0424d9706892e34362b4d1f99
liyaSileshi <liyasileshi@gmail.com>
2019-12-05 21:25:17 -0800

    2 models working

--- book_rater/rater/models.py
+++ book_rater/rater/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length = 200)
-    description = models.TextField()
+    title = models.CharField(max_length = 200, default='book title', null=True)
+    description = models.TextField(default='book description')
 
 class Comment(models.Model):
-    
+    comment = models.TextField(default='book comment')
+    book = models.ForeignKey(Book, on_delete = models.CASCADE)
