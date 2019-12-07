8986c3384cf935126fa84837b319cfd183634dc6
liyaSileshi <liyasileshi@gmail.com>
2019-12-07 14:42:46 -0800

    added styles and models

--- book_rater/rater/forms.py
+++ book_rater/rater/forms.py
@@ -3,20 +3,21 @@ from rater.models import Comment
 from rater.models import Book
 from django.forms import ModelForm, TextInput
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = [
-                'title'
-            ]
-        widgets = {
-            'title': TextInput(attrs={'class' : 'input', 'name':'q', 'placeholder' : 'Book Name'}),
-        }
+# class BookForm(forms.ModelForm):
+#     class Meta:
+#         model = Book
+#         fields = [
+#                 'title'
+#             ]
+#         widgets = {
+#             'title': TextInput(attrs={'class' : 'input', 'name':'q', 'placeholder' : 'Book Name'}),
+#         }
 
 class CommentForm(forms.ModelForm):
     """ Render and process a form based on the Comment model. """
     class Meta:
         model = Comment
         fields = [
-                'comment'
+                'comment',
+                'rating'
             ]
--- book_rater/rater/views.py
+++ book_rater/rater/views.py
@@ -6,50 +6,50 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
 from django.urls import reverse, reverse_lazy
-from rater.forms import CommentForm, BookForm
+from rater.forms import CommentForm
 import requests
 import json
 # Create your views here.
 
 class BookListView(ListView):
     """ Renders a list of all Books. """
-    # model = Book
-    # def get(self, request):
-    #     """ GET a list of Books. """
-    #     books = self.get_queryset().all()
-    #     return render(request, 'rater/index.html', {
-    #       'books': books
-    #     })
-
+    model = Book
     def get(self, request):
-        name = request.GET.get('q', '')
-        url = 'https://www.googleapis.com/books/v1/volumes?q={}'
-        # response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={}')
-        book_db = Book.objects.all()
-        # name = 'Bad Blood'
-        book_array = {}
-        form = BookForm()
-        books = requests.get(url.format(name)).json() #request the API data and convert the JSON to Python data types
-        print(books)
-        # json.data = json.loads(books['items'])
-        # print(json.data)
-        results = books['items']
+        """ GET a list of Books. """
+        books = self.get_queryset().all()
+        return render(request, 'rater/index.html', {
+          'books': books
+        })
 
-        # for book in book_db:
-        
-        # print(results)
-        for book in results:
-            # book_array.append(book['volumeInfo']['imageLinks']['thumbnail'])
-            book_array[book['id']] = book['volumeInfo']['imageLinks']['thumbnail']
-            # book['id']
-        print(book_array)
-        context = {'book_array':book_array, 'form':form}
-        # print(context)
-        
-        return render(request, 'rater/index.html', context) #returns the index.html template
+    # def get(self, request):
+    #     name = request.GET.get('q', '')
+    #     url = 'https://www.googleapis.com/books/v1/volumes?q={}'
+    #     # response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={}')
+    #     book_db = Book.objects.all()
+    #     name = 'Bad Blood'
+    #     book_array = {}
+    #     form = BookForm()
+    #     books = requests.get(url.format(name)).json() #request the API data and convert the JSON to Python data types
+    #     print(books)
+    #     # json.data = json.loads(books['items'])
+    #     # print(json.data)
+    #     results = books['items']
 
-    def post(self, request):
-        pass
+    #     # for book in book_db:
+        
+    #     # print(results)
+    #     for book in results:
+    #         # book_array.append(book['volumeInfo']['imageLinks']['thumbnail'])
+    #         book_array[book['id']] = book['volumeInfo']['imageLinks']['thumbnail']
+    #         # book['id']
+    #     print(book_array)
+    #     context = {'book_array':book_array, 'form':form}
+    #     # print(context)
+        
+    #     return render(request, 'rater/index.html', context) #returns the index.html template
+
+    # def post(self, request):
+    #     pass
 
 class BookDetailView(DetailView):
     """ Renders a specific page based on it's slug."""
@@ -69,11 +69,11 @@ class BookDetailView(DetailView):
         book = self.get_queryset().get(slug__iexact=slug)
         form = CommentForm(request.POST)
         if form.is_valid():
-          print(form.cleaned_data)
+        #   print(form.cleaned_data)
           comment = form.save(commit=False)
           comment.book = book
           comment.save()
-          print(comment)
+        #   print(comment)
           return HttpResponseRedirect(reverse('rater:detail',args=(slug,)))
 
 class CommentDetail(DetailView):
